78c7437e2224f21419be245e8e3e746c9dcd7870
王清柏 <wangcbstudy@gmai.com>
2026-07-02 20:05:58 +0800

    上一個部署有問題，所以加了「我要使用 Workflow，也就是部署到 GitHub Pages」這些字詞

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,13 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
+  const isProd = process.env.NODE_ENV === 'production';
+  const repoName = process.env.GITHUB_REPOSITORY 
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` 
+    : '/';
+
   return {
-    base: process.env.NODE_ENV === 'production' ? '/__Drink_Ordering_System__/' : '/',
+    base: isProd ? repoName : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
